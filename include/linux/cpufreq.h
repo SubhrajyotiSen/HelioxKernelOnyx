1a67c584c8b43394a942eb99a173dfa1cff27160
andip71 <andreasp@gmx.de>
2015-08-04 13:11:10 +0200

    cpufreq: Add smartmax eps (extreme power saving) governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -401,6 +401,9 @@ extern struct cpufreq_governor cpufreq_gov_wheatley;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX_EPS)
+extern struct cpufreq_governor cpufreq_gov_smartmax_eüs;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartmax_eps)
 #endif
 
 /*********************************************************************
