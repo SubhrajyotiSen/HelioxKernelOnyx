d916625ba702a2d9b98557adf9a75b98adbcf86c
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2016-11-10 00:55:19 +0530

    Add zipping to script

--- heliox.sh
+++ heliox.sh
@@ -2,6 +2,14 @@
 export ARCH=arm
 export CROSS_COMPILE="/home/subhrajyoti/kernel/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-"
 make clean
+echo "Enter version"
+read version
 make cm_onyx_defconfig
 make oldconfig
 make -j4
+echo "------Heliox Built------"
+yes | cp -rf ./arch/arm/boot/zImage-dtb ../AnyKernel2/
+cd ../AnyKernel2
+zip -r9 Heliox-"$version".zip * -x README Heliox-"$version".zip
+
+
